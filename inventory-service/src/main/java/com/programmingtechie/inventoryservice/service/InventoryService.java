3890a6b37707c277825f50083d8472a7fc37d666
issalo1998 <issalo163@gmail.com>
2023-02-23 18:40:23 +0000

    first commit

---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,35 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+//@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+  //  @SneakyThrows //Don't use this in production
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        //log.info("starting");
+       // Thread.sleep(10000);
+       // log.info("end");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).collect(Collectors.toList());
+    }
+}
